ed671263a795ce3c1b843b057de22cfd539009ae
Little-monster <1580300779.qq.com>
2021-05-23 21:14:02 +0800

    404/错误处理中间件修改

--- controllers/notFound/notFound_handle.js
+++ controllers/notFound/notFound_handle.js
@@ -1,4 +1,8 @@
 module.exports = (req, res, next) => {
   // NotFound
-  res.status(404).send("抛出错误：" + "NotFound")
+  if(err.message) {
+    res.status(404).send("抛出错误：" + err.message)
+  } else {
+    res.status(404).send("抛出错误：" + "NotFound")
+  }
 }
